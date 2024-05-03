64a7771c438fbc36c6eeff706403df63ba935a11
ilya_gasan <ilya.gasan2019@mail.ru>
2024-05-03 13:43:02 +0300

    added datepicker  and counters

--- avia_sales/src/main.js
+++ avia_sales/src/main.js
@@ -8,8 +8,10 @@ import router from './router'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 import 'normalize.css'
+import VueDatePicker from '@vuepic/vue-datepicker';
+import '@vuepic/vue-datepicker/dist/main.css'
 const app = createApp(App)
-
+app.component('VueDatePicker', VueDatePicker);
 app.use(createPinia())
 app.use(router)
 
